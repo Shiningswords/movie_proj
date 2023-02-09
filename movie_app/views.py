6fe77de532a54be91dc94d4d4468c05989300f65
GudochkinK <goodok-01@hotmail.com>
2023-02-09 17:13:59 +0300

    использование функций агрегирования данных

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.db.models import Sum, Max, Min, Count, Avg
 from .models import Movie
 
 
 def show_all_movie(request):
     movies = Movie.objects.order_by('name')
-    return render(request, 'movie_app/all_movies.html', context={'movies': movies})
+    agg = movies.aggregate(Avg('budget'), Min('rating'), Max('rating'), Count('id'))
+    return render(request, 'movie_app/all_movies.html', context={'movies': movies,
+                                                                 'agg': agg})
 
 
 def show_one_movie(request, slug_movie: str):
