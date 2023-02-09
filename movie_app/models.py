4c0215d569adba2ef3bcd745055c6832b968df51
GudochkinK <goodok-01@hotmail.com>
2023-02-09 16:50:21 +0300

    Добавление slug поля в модель Movie

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.utils.text import slugify
 
 
 class Movie(models.Model):
@@ -7,9 +8,14 @@ class Movie(models.Model):
     rating = models.IntegerField()
     year = models.IntegerField(null=True)
     budget = models.IntegerField(default=1000000)
+    slug = models.SlugField(default='', null=False, db_index=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Movie, self).save(*args, **kwargs)
 
     def get_url(self):
-        return reverse('one_movie', args=[self.id])
+        return reverse('one_movie', args=[self.slug])
 
     def __str__(self):
         return f'{self.name} - {self.rating}'
