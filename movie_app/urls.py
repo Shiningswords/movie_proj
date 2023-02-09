cda33a55fb12edadcd6b35555267458fc9d10e06
GudochkinK <goodok-01@hotmail.com>
2023-02-09 15:46:51 +0300

    прописывание роута и создание шаблона с информацие об одном фильме

--- movie_app/urls.py
+++ movie_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.show_all_movie)
+    path('', views.show_all_movie),
+    path('movie/<int:id_movie>', views.show_one_movie)
 ]
