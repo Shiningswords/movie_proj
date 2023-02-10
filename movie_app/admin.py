2daca15195bced41d0d6ab92c9399669319c4530
GudochkinK <goodok-01@hotmail.com>
2023-02-10 17:21:34 +0300

    создание своего фильтра для панели админа

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -3,6 +3,30 @@ from .models import Movie
 from django.db.models import QuerySet
 
 
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фильтр по рейтингу'
+    parameter_name = 'rate'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Low'),
+            ('41-60', 'Middle'),
+            ('61-85', 'High'),
+            ('86-100', 'Top')
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=41)
+        elif self.value() == '41-60':
+            return queryset.filter(rating__lt=61).filter(rating__gt=40)
+        elif self.value() == '61-85':
+            return queryset.filter(rating__lt=86).filter(rating__gt=60)
+        elif self.value() == '86-100':
+            return queryset.filter(rating__lt=101).filter(rating__gt=85)
+
+
+
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
     list_display = ['name', 'rating', 'year', 'currency', 'rating_status']
@@ -11,7 +35,7 @@ class MovieAdmin(admin.ModelAdmin):
     list_per_page = 4
     actions = ['set_dollars', 'set_euro']
     search_fields = ['name']
-    list_filter = ['currency']
+    list_filter = ['currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, mov):
