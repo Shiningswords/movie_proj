7771470ec93f68aab87886ff2b5914b7e993960f
GudochkinK <goodok-01@hotmail.com>
2023-02-10 16:52:13 +0300

    добавление поиска в панели админа

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -10,6 +10,7 @@ class MovieAdmin(admin.ModelAdmin):
     ordering = ['rating']
     list_per_page = 4
     actions = ['set_dollars', 'set_euro']
+    search_fields = ['name']
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, mov):
